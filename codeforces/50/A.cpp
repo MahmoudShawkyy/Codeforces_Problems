f6b0b63014474c3d388f4a0446f6a7440fca48bf
Mahmoud Shawky <MahmoudShawky2250@gmail.com>
2021-05-02 17:20:00 +0200

    Add solution for problem `A - Domino piling` Link: https://codeforces.com/contest/50/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/50/submission/114908241

---
+++ codeforces/50/A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+typedef long long ll;
+#define PB push_back
+#define len(s) (int)s.length()
+#define sz(v)  (int)v.size()
+#define all(a)  a.begin(),a.end()
+#define array(arr)  arr,arr+n
+#define endl '\n'
+#define loop(n) for(int i = 0; i < (n); i++)
+int main()
+{
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+int m,n;
+cin>>m>>n;
+cout<<(m*n)/2<<endl;
+
+    return 0;
+}
