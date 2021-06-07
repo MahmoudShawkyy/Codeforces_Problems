3185031a40999d49122dad8964ec51f8d3727db6
Mahmoud Shawky <MahmoudShawky2250@gmail.com>
2021-06-08 00:00:00 +0200

    Add solution for problem `A - Calculating Function` Link: https://codeforces.com/contest/486/problem/A Tags: implementation, math, *800 Ref: https://codeforces.com/contest/486/submission/118758596

---
+++ codeforces/486/A.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+typedef long long ll;
+#define PB push_back
+#define len(s) (int)s.length()
+#define sz(v)  (int)v.size()
+#define all(v)  v.begin(),v.end()
+#define array(arr)  arr,arr+n
+#define endl '\n'
+#define loop(n) for(int i = 0; i < (n); i++)
+#define loop2(n) for(int j = 0; j < (n); j++)
+
+void solve()
+{
+    ll n1,n2,n;
+    cin>>n;
+    if(n%2==0)
+     cout<<n/2;
+    else
+     cout<<-(n+1)/2;
+}
+int main()
+{
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+    /*int t;
+    cin>>t;
+    while(t--)*/
+
+    solve();
+
+}
