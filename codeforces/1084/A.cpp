d572c3e38ff3410eec24e57ac996ff22a0e1347f
Mahmoud Shawky <MahmoudShawky2250@gmail.com>
2021-08-06 04:40:00 +0200

    Add solution for problem `A - The Fair Nut and Elevator` Link: https://codeforces.com/contest/1084/problem/A Tags: brute force, implementation, *1000 Ref: https://codeforces.com/contest/1084/submission/125008477

---
+++ codeforces/1084/A.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+typedef long long ll;
+#define PB push_back
+#define len(s) (int)s.length()
+#define sz(v)  (int)v.size()
+#define all(v)  v.begin(),v.end()
+#define array(arr)  arr,arr+n
+#define endl '\n'
+#define loop(n) for(int i = 0; i < (n); i++)
+#define loop2(n) for(int j = 0; j < (n); j++)
+//int freq[20,0005]={0};
+void solve()
+{ 
+    int n,m,sum=0;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cin>>m;
+        sum+=(m*4*i);
+    }
+    cout<<sum<<endl;
+}
+int main()
+{
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+    int t=1;//cin>>t;
+    while(t--)
+        solve();
+
+}
