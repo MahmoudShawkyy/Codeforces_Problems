c0bf000936fc180c9b365cdfa7ddef2646e25692
Mahmoud Shawky <MahmoudShawky2250@gmail.com>
2021-08-07 02:00:00 +0200

    Add solution for problem `A - HQ9+` Link: https://codeforces.com/contest/133/problem/A Tags: implementation, *900 Ref: https://codeforces.com/contest/133/submission/125095888

---
+++ codeforces/133/A.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+typedef long long ll;
+#define PB push_back
+#define len(s) (int)s.length()
+#define sz(v)  (int)v.size()
+#define all(v)  v.begin(),v.end()
+#define array(arr)  arr,arr+n
+#define endl '\n'
+#define loop(n) for(int i = 0; i < (n); i++)
+#define loop2(n) for(int j = 0; j < (n); j++)
+int solve()
+{
+   string s;
+   cin>>s;
+   loop(len(s))
+   {
+       if(s[i]=='H'||s[i]=='Q'||s[i]=='9')
+       {
+           cout<<"YES\n";
+           return 0;
+       }
+   }
+   cout<<"NO\n";
+}
+int main()
+{
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+    int t=1;//cin>>t;
+    while(t--)
+        solve();
+
+}
