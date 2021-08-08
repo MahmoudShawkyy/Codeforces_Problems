95eb69f6082ecac0200f07bff3310b83f19e2ad7
Mahmoud Shawky <MahmoudShawky2250@gmail.com>
2021-08-08 18:20:00 +0200

    Add solution for problem `B - Vasya and Books` Link: https://codeforces.com/contest/1073/problem/B Tags: implementation, math, *1000 Ref: https://codeforces.com/contest/1073/submission/125235103

---
+++ codeforces/1073/B.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+typedef long long ll;
+#define PB push_back
+#define len(s) (int)s.length()
+#define sz(v)  (int)v.size()
+#define all(v)  v.begin(),v.end()
+#define array(arr)  arr,arr+n
+#define endl '\n'
+#define loop(n) for(int i = 0; i < (n); i++)
+#define loop2(n) for(int j = 0; j < (n); j++)
+int freq[200005]={0};
+void solve()
+{
+ int n,m,cnt=0;
+ cin>>n;
+ for(int i=1;i<=n;i++)
+ {
+        cin>>m;
+        freq[m]=i;
+ }
+  for(int i=1;i<=n;i++)
+ {
+        cin>>m;
+        cout<<max(freq[m]-cnt,0)<<" ";
+        cnt+=max(freq[m]-cnt,0);
+ }
+}
+int main()
+{
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+    solve();
+    return 0;
+}
