dbf2503bfeb21102978c7a244d277cc1d1a86765
Mahmoud Shawky <MahmoudShawky2250@gmail.com>
2021-08-15 02:58:00 +0200

    Add solution for problem `A - Cheap Travel` Link: https://codeforces.com/contest/466/problem/A Tags: implementation, *1200 Ref: https://codeforces.com/contest/466/submission/125888486

---
+++ codeforces/466/A.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+typedef long long ll;
+#define PB push_back
+#define len(s) (int)s.length()
+#define sz(v)  (int)v.size()
+#define all(v)  v.begin(),v.end()
+#define array(arr)  arr,arr+n
+#define endl '\n'
+#define loop(n) for(int i = 0; i < (n); i++)
+#define loop2(n) for(int j = 0; j < (n); j++)
+
+void solve()
+{
+    int n,m,a,b,res,ans,num;
+    cin>>n>>m>>a>>b;
+    res = (n/m)*b+(n%m)*a;
+    num=ceil((double)n/m)*b;
+     ans = min(num,n*a);
+   cout<<min(res,ans)<<endl;
+}
+int main()
+{
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+    int t=1;//cin>>t;
+    while(t--)
+        solve();
+    return 0;
+}
