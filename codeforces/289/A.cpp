8444671b54332b6d40538ce4e0b7db78b77a31d8
Mahmoud Shawky <MahmoudShawky2250@gmail.com>
2021-09-11 15:13:00 +0200

    Add solution for problem `A - Polo the Penguin and Segments ` Link: https://codeforces.com/contest/289/problem/A Tags: brute force, implementation, *1100 Ref: https://codeforces.com/contest/289/submission/128507593

---
+++ codeforces/289/A.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#include <queue>
+using namespace std;
+typedef vector<int> vi;
+typedef long long ll;
+#define pb push_back
+#define len(s) (int)s.length()
+#define sz(v)  (int)v.size()
+#define all(v)  v.begin(),v.end()
+#define array(arr)  arr,arr+n
+#define endl '\n'
+#define rep(k,n,x) for(int i=k;i<n;i+=x)
+#define loop(n) for(int i=0;i<n;i++)
+#define loop2(m) for(int j=0;j<m;j++)
+void solve()
+{
+int n,k,a,b;
+ll ans=0;
+cin>>n>>k;
+loop(n)
+{
+    cin>>a>>b;
+   ans+=abs(b-a)+1;
+}
+if(ans%k)
+cout<<k-(ans%k)<<endl;
+else
+    cout<<0<<endl;
+}
+int main()
+{
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+    int t=1;
+    //cin>>t;
+    while(t--)
+        solve();
+
+    return 0;
+}
