2981802aed16b3acd82b5b349001af361cff713b
Mahmoud Shawky <MahmoudShawky2250@gmail.com>
2021-09-13 02:21:00 +0200

    Add solution for problem `A - Balanced Rating Changes` Link: https://codeforces.com/contest/1237/problem/A Tags: implementation, math, *1000 Ref: https://codeforces.com/contest/1237/submission/128683188

---
+++ codeforces/1237/A.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#include <queue>
+using namespace std;
+typedef vector<int> vi;
+typedef long long ll;
+#define pb push_back
+#define len(s) (int)s.length()
+#define sz(v)  (int)v.size()
+#define all(v)  v.begin(),v.end()
+#define array(arr)  arr,arr+n
+#define endl '\n'
+#define rep(k,n,x) for(int i=k;i<n;i+=x)
+#define loop(n) for(int i=0;i<n;i++)
+#define loop2(m) for(int j=0;j<m;j++)
+void solve()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    bool flag=true;
+    loop(n)
+    {
+        cin>>arr[i];
+        if(abs(arr[i])%2!=0&&flag)
+    {
+        arr[i]
+            --;
+            arr[i]/=2;
+            flag=false;
+    }
+        else if(abs(arr[i])%2!=0&&!flag)
+    {
+        arr[i]
+            ++;
+            arr[i]/=2;
+            flag=true;
+        }
+        else
+            arr[i]/=2;
+        }
+    loop(n)
+    cout<<arr[i]<<endl;
+}
+int main()
+{
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+    int t=1;
+    //cin>>t;
+    while(t--)
+        solve();
+
+    return 0;
+}
